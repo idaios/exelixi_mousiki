b545f977727f04865492be69d6f2fd1c5465bfbc
idaios <pavlidisp@gmail.com>
2024-02-07 23:22:05 +0200

    songs to sequences

--- audio_analysis.py
+++ audio_analysis.py
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import re
-
+import random
 data = {}
 sr=22000
 
 #audio = librosa.util.example_audio_file()
 pattern = re.compile(r'psarantonis|skordalos|hpeirwtika')
-#pattern2 = re.compile(r'hpeirwtika')
+pattern2 = re.compile(r'skordalos|psarantonis')
 
 
 features = {}
@@ -28,7 +28,7 @@ for f in fileList:
         mp3list.append(f)
 
 mp3list
-        
+
 
 for file in mp3list:
     if file.endswith(".mp3"):
@@ -50,12 +50,8 @@ for file in mp3list:
         beat_times = librosa.frames_to_time(beat_frames, sr=sr)
         beat_time_diff=np.ediff1d(beat_times)
         beat_nums = np.arange(1, np.size(beat_times))
-        ##features[file]['beat_times'] = beat_times
-        ##features[file]['beat_time_diff'] = beat_time_diff
-        ##features[file]['beat_nums'] = beat_nums
         ## chroma
         chroma=librosa.feature.chroma_cens(y=y_harmonic, sr=sr)
-        ##features[file]['chroma'] = chroma
         ## mfccs
         mfccs = librosa.feature.mfcc(y=y_harmonic, sr=sr, n_mfcc=13)
         ##features[file]['mfccs'] = mfccs
@@ -93,8 +89,12 @@ for file in mp3list:
 
 ll=list(features.keys())
 
+keysToUse=[]
 features_df = pd.DataFrame()
 for song in ll:
+    if not pattern2.match(song):
+        continue
+    keysToUse.append(song)
     print(song)
     f0 = song
     newrow={}
@@ -154,3 +154,172 @@ fig.show()
 fig2 = px.scatter(components, x=0, y=1, color=singer, text=df.index)
 fig2.show()
 
+
+############### play with one song
+
+### https://www.educative.io/answers/how-to-perform-note-tracking-in-librosa
+### Get the notes and their duration
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+# sphinx_gallery_thumbnail_path = '_static/playback-thumbnail.png'
+import librosa
+# We'll need IPython.display's Audio widget
+from IPython.display import Audio
+# We'll also use `mir_eval` to synthesize a signal for us
+import mir_eval.sonify
+
+
+allnotes={}
+alldurations={}
+for audio_file in mp3list:
+    # Loading the audio file 
+    y, sr = librosa.load(audio_file)
+    # Extracting the chroma features and onsets
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    chroma = librosa.feature.chroma_stft(y=y_harmonic, sr=sr)
+    onset_frames = librosa.onset.onset_detect(y=y_harmonic, sr=sr)
+    first = True
+    notes = []
+    mypitch=[]
+    mydurations=[]
+    for onset in onset_frames:
+        chroma_at_onset = chroma[:, onset]
+        note_pitch = chroma_at_onset.argmax()
+        # For all other notes
+        if not first:
+            note_duration = librosa.frames_to_time(onset, sr=sr)
+            notes.append((note_pitch,onset, note_duration - prev_note_duration))
+            prev_note_duration = note_duration
+            # For the first note
+        else:
+            prev_note_duration = librosa.frames_to_time(onset, sr=sr)
+            first = False
+    print("Note pitch \t Onset frame \t Note duration")
+    for entry in notes:
+        print(entry[0],'\t\t',entry[1],'\t\t',entry[2])
+        mypitch.append(entry[0])
+        mydurations.append(entry[2])
+    allnotes[audio_file] = mypitch
+    alldurations[audio_file] = mydurations
+    len(notes)
+
+notes[0]
+
+[len(i) for i in allnotes.values()]
+allnotes['psarantonis1.mp3'][:20]
+
+for i in allnotes.keys():
+    print(f">{i}")
+    for j in range(len(allnotes[i])):
+        print(chr(allnotes[i][j]+65), end="")
+    print()
+    
+
+
+
+## APPROACH 3 without onset but split time
+    
+############### play with one song
+
+### https://www.educative.io/answers/how-to-perform-note-tracking-in-librosa
+### Get the notes and their duration
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+# sphinx_gallery_thumbnail_path = '_static/playback-thumbnail.png'
+import librosa
+# We'll need IPython.display's Audio widget
+from IPython.display import Audio
+# We'll also use `mir_eval` to synthesize a signal for us
+import mir_eval.sonify
+
+audio_file
+
+allnotes={}
+alldurations={}
+nres = 1000
+resolution = 24
+for audio_file in mp3list:
+    # Loading the audio file 
+    y, sr = librosa.load(audio_file)
+    # Extracting the chroma features and onsets
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    ##   totalDuration = librosa.get_duration(y=y, sr=sr)
+    ##  totalDuration
+    ##  len(y_harmonic)
+    ##  step = len(y_harmonic)//nres - 1
+    ##  j = step
+    ##    while j < len(y_harmonic):
+
+    chroma = librosa.feature.chroma_stft(y=y_harmonic, sr=sr, n_fft=2048, hop_length=256, win_length=2048, n_chroma=24)
+    onset_frames = librosa.onset.onset_detect(y=y_harmonic, sr=sr)
+    first = True
+    notes = []
+    mypitch=[]
+    mydurations=[]
+    #    chroma.shape
+    #    len(onset_frames)
+    for onset in range(chroma.shape[1]): #onset_frames:
+        chroma_at_onset = chroma[:, onset]
+        note_pitch = chroma_at_onset.argmax()
+        # For all other notes
+        if not first:
+            note_duration = librosa.frames_to_time(onset, sr=sr)
+            notes.append((note_pitch,onset, note_duration - prev_note_duration))
+            prev_note_duration = note_duration
+            # For the first note
+        else:
+            prev_note_duration = librosa.frames_to_time(onset, sr=sr)
+            first = False
+    print("Note pitch \t Onset frame \t Note duration")
+    for entry in notes:
+        print(entry[0],'\t\t',entry[1],'\t\t',entry[2])
+        mypitch.append(entry[0])
+        mydurations.append(entry[2])
+    allnotes[audio_file] = mypitch
+    alldurations[audio_file] = mydurations
+    len(notes)
+
+notes[0]
+
+[len(i) for i in allnotes.values()]
+allnotes['psarantonis1.mp3'][:20]
+
+countNotesDif=[]
+countNotesDifFrequencies=[]
+corfactor = 65 + resolution
+with open("songs.fa", "w") as outf:
+    for i in allnotes.keys():
+        cur_note = ""
+        prev_note = chr(allnotes[i][0]+65)
+        outf.write(f">{i}\n")
+        jj = 0
+        for j in range(1,len(allnotes[i])):
+            dif = allnotes[i][j] - allnotes[i][j-1] + corfactor
+            cur_note = chr(allnotes[i][j]+65)
+            if prev_note != cur_note:
+                countNotesDif.append(dif)
+                if dif == 89:
+                    print([prev_note, cur_note, dif])
+                #print(chr(allnotes[i][j]+65), end="")
+                outf.write(f"{chr(dif)}")
+            prev_note = cur_note
+            jj+=1
+        outf.write("\n")
+
+mycntdifs = Counter(countNotesDif)
+
+
+nrandomsongs = 200
+randomlength=2000
+with open("randomDifsongs.fa", "w") as outf:
+    for i in range(nrandomsongs):
+        outf.write(f">random{i}\n")
+        seq = random.choices([chr(k) for k in mycntdifs.keys()], weights=list(mycntdifs.values()), k=randomlength)
+        outf.write(''.join(seq))
+        outf.write("\n")
+                   
